fc8d40e856f43baf34bd4e22f33fe2353ac20678
ojedajda <ojedadanieljd@gmail.com>
2024-04-16 10:02:09 -0500

    commit 18

---
+++ scrip.js
@@ -0,0 +1,8 @@
+let botonesAgregar = document.querySelectorAll('.agregar');
+botonesAgregar.forEach(function(boton) {
+    boton.addEventListener('click', function() {
+        let idProducto = this.getAttribute('data-id');
+        
+        
+    });
+});
